d2f317c1277a45f3d60f9212151700f7c9323138
Lucas <lucasbertoncello1@gmail.com>
2025-05-02 17:01:16 -0300

    feat: hook to switch color theme

---
+++ src/hooks/useToggleTheme.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function useToggleTheme() {
+  const [theme, setTheme] = useState<string>("dark");
+
+  const toggleTheme = () =>
+    setTheme((prevState: string) => (prevState === "dark" ? "light" : "dark"));
+
+  useEffect(() => {
+    const root = document.documentElement;
+    console.log(root);
+
+    return theme === "dark"
+      ? root.classList.add("dark")
+      : root.classList.remove("dark");
+  }, [theme]);
+
+  return { toggleTheme };
+}
