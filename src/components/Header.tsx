f485d0f778c1bffa4d2fcfb6761537096b97f5c9
Lucas <lucasbertoncello1@gmail.com>
2025-05-02 20:17:08 -0300

    feat: mobile button on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
-import { Link } from "react-router-dom";
 import Container from "./Container";
 
+import { Link } from "react-router-dom";
+import { Menu } from "lucide-react";
+import { useState } from "react";
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="dark:bg-dark-900 py-10">
+    <header className="dark:bg-dark-900 py-10 relative">
       <Container>
         <div className="flex justify-between items-center">
           {/* logo */}
@@ -16,8 +21,8 @@ export default function Header() {
             </Link>
           </div>
 
-          {/* menu */}
-          <ul className="dark:text-font-dark flex gap-10 font-medium">
+          {/* menu desktop */}
+          <ul className="dark:text-font-dark hidden lg:flex gap-10 font-medium">
             <li className="dark:hover:text-white transition-all">
               <Link to={"/about"}>Sobre</Link>
             </li>
@@ -33,7 +38,41 @@ export default function Header() {
               </a>
             </li>
           </ul>
+
+          {/* menu mobile btn */}
+          <Menu
+            onClick={() => setIsOpen((prevState) => !prevState)}
+            className="lg:hidden cursor-pointer"
+            size={32}
+          />
         </div>
+        {isOpen && (
+          <ul
+            className="
+          dark:bg-dark-800 h-[400px] w-full max-w-[800px] 
+          absolute top-[300px] left-1/2 -translate-x-1/2 -translate-y-1/2
+          flex
+          items-center
+          justify-around
+          flex-col
+          "
+          >
+            <li className="dark:hover:text-white transition-all">
+              <Link to={"/about"}>Sobre</Link>
+            </li>
+            <li className="dark:hover:text-white transition-all">
+              <Link to={"/projects"}>Projetos</Link>
+            </li>
+            <li className="dark:hover:text-white transition-all">
+              <Link to={"/contact"}>Contato</Link>
+            </li>
+            <li className="dark:hover:text-main-dark transition-all">
+              <a href="../CV.pdf" download>
+                Baixar CV
+              </a>
+            </li>
+          </ul>
+        )}
       </Container>
     </header>
   );
