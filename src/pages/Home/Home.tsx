fb4d7ebb0c36a7053d8dc4f19138a130971434f7
Lucas <lucasbertoncello1@gmail.com>
2025-05-04 12:16:14 -0300

    chore: animate on scroll setup

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,8 @@ import ExperienceArea from "./components/ExperienceArea";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 
+import AOS from "aos";
+
 export default function Home() {
   return (
     <main className="pt-40">
