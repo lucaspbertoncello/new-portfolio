a378de37e1e412960c16a7a34a95426fae8a6949
Lucas <lucasbertoncello1@gmail.com>
2025-05-04 12:20:22 -0300

    feat: animate on scroll to components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import HeroArea from "./components/HeroArea";
 import AboutArea from "./components/AboutArea";
 import ExperienceArea from "./components/ExperienceArea";
@@ -5,14 +7,34 @@ import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 
 import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
     <main className="pt-40">
-      <HeroArea />
-      <AboutArea />
-      <ExperienceArea />
-      <Skills />
+      <div data-aos="fade-right">
+        <HeroArea />
+      </div>
+
+      <div data-aos="fade-left">
+        <AboutArea />
+      </div>
+
+      <div data-aos="fade-right">
+        <ExperienceArea />
+      </div>
+
+      <div data-aos="fade-left">
+        <Skills />
+      </div>
+
       <Footer />
     </main>
   );
