e5b36894ba7c40e9054eba1641f61f5e58a52b51
Lucas <lucasbertoncello1@gmail.com>
2025-05-05 16:42:32 -0300

    feat: new projects

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -1,5 +1,7 @@
 import moldPar from "../assets/images/moldpar.png";
 import myContacts from "../assets/images/mycontacts.png";
+import getkcal from "../assets/images/getkcal.png";
+import burgerLp from "../assets/images/burger-lp.png";
 
 export const projects = [
   {
@@ -26,5 +28,23 @@ export const projects = [
     ],
     description:
       "Este é um projeto Full Stack utilizando React no front-end e Node.js com Express no back-end. A aplicação permite o gerenciamento de uma lista de contatos e categorias, com operações completas de Criar, Ler, Atualizar e Excluir (CRUD).",
+  },
+  {
+    image: burgerLp,
+    projectName: "Burguer",
+    projectLink: "",
+    githubLink: "https://github.com/lucaspbertoncello/burguer-landing-page",
+    techStack: ["Html", "Css"],
+    description:
+      "Landing page estática criada para venda de hambúrgers, realizada para fins de aprendizado.",
+  },
+  {
+    image: getkcal,
+    projectName: "getkcal",
+    projectLink: "",
+    githubLink: "https://github.com/lucaspbertoncello/getkcal",
+    techStack: ["Html", "Css", "JavaScript"],
+    description:
+      "Projeto simples criado com JavaScript, para medir o índice de massa corporal (IMC), com base nos dados fornecidos no formulário da aplicação",
   },
 ];
