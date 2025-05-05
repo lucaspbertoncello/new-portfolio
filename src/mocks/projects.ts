decf1fc73fd17885f61b499a59ce6029a4658632
Lucas <lucasbertoncello1@gmail.com>
2025-05-05 04:56:50 -0300

    feat: mock to store my projects

---
+++ src/mocks/projects.ts
@@ -0,0 +1,12 @@
+import moldPar from "../assets/images/moldpar.png";
+
+export const projects = [
+  {
+    image: moldPar,
+    projectName: "MoldPar",
+    githubLink: "",
+    techStack: ["Html", "Css", "JavaScript"],
+    description:
+      "A real-time coaching app for students learning to paint. This app is my baby, designed and built on my own.",
+  },
+];
