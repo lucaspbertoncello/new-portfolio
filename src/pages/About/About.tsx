ff0579386df1ae008699e1f3bd83a17ceb73e7d4
Lucas <lucasbertoncello1@gmail.com>
2025-05-05 03:17:33 -0300

    feat: finished about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,3 +1,26 @@
+import Container from "../../components/Container";
+import Title from "../../components/Title";
+
 export default function About() {
-  return <h1>about</h1>;
+  return (
+    <main className="my-20">
+      <Container>
+        <Title text="Sobre mim" />
+
+        <p className="text-lg leading-[30px]">
+          <span className="dark:text-main-dark text-main-light">Lorem</span>{" "}
+          ipsum dolor sit amet consectetur adipisicing elit. Veritatis ab
+          doloremque vel error ut, unde dolore nobis neque autem nesciunt velit
+          magnam consequuntur repellat dolores nihil, mollitia cupiditate
+          suscipit ipsam. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Veritatis ab doloremque vel error ut, unde dolore nobis neque
+          autem nesciunt velit magnam consequuntur repellat dolores nihil,
+          mollitia cupiditate suscipit ipsam. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Veritatis ab doloremque vel error ut,
+          unde dolore nobis neque autem nesciunt velit magnam consequuntur
+          repellat dolores nihil, mollitia cupiditate suscipit ipsam.
+        </p>
+      </Container>
+    </main>
+  );
 }
